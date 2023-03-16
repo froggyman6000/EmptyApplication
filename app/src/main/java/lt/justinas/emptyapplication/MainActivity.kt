6ce2e92356a0aa15164a18d116672e2fd99f65fe
froggyman6000 <trunce123@gmail.com>
2023-03-16 15:31:16 +0200

    Change openButton visibility

--- app/src/main/java/lt/justinas/emptyapplication/MainActivity.kt
+++ app/src/main/java/lt/justinas/emptyapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -18,14 +19,17 @@ class MainActivity : AppCompatActivity() {
         val myTextView: TextView = findViewById(R.id.myTextView)
         val openButton: Button = findViewById(R.id.buttonOpen)
         Log.i("myTag", "onCreate: ${myTextView.text}")
+        openButton.isVisible = false // instead of this we can use openButton.visibility = View.INVISIBLE
 
         lifecycleScope.launch{
             myTextView.text = getString(R.string.sayHello)
             Log.i("myTag", "onCreate: Changed text to ${myTextView.text}")
 
             delay(5000)
+
             myTextView.text = getString(R.string.secondMessage)
-            Log.i("myTag", "onCreate: Waited 5 seconds and changed text to ${myTextView.text}")
+            openButton.isVisible = true
+            Log.i("myTag", "onCreate: Waited 5 seconds and changed text to ${myTextView.text}, also made the button visible")
 
         }
 
