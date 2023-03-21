35cfbb6047f93caf364fe312025642089304a871
froggyman6000 <trunce123@gmail.com>
2023-03-21 13:50:46 +0200

    Add TextView and Button to Second Activity

--- app/src/main/java/lt/justinas/emptyapplication/SecondActivity.kt
+++ app/src/main/java/lt/justinas/emptyapplication/SecondActivity.kt
@@ -2,10 +2,19 @@ package lt.justinas.emptyapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val secondTextView: TextView = findViewById(R.id.secondTextView)
+        val secondButton: Button = findViewById(R.id.secondButton)
+
+        secondTextView.text = getString(R.string.secondActivityText)
+        secondButton.text = getString(R.string.secondButtonText)
+
     }
 }
